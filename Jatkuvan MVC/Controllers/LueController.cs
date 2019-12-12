4bce817db3c658ca8fcab7acc8306fee45f3110a
Joushy <johannakristiinasaar@gmail.com>
2019-12-12 17:16:55 +0200

    jeee

---
+++ Jatkuvan MVC/Controllers/LueController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Jatkuvan_MVC.Models;
+
+namespace Jatkuvan_MVC.Controllers
+{
+    public class LueController : Controller
+    {
+        private readonly JatkuvaTehtavaContext _context = new JatkuvaTehtavaContext();
+
+        public LueController()
+        {
+            
+        }
+
+        // GET: Lue
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Topic.ToListAsync());
+        }
+
+        // GET: Lue/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topic
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return View(topic);
+        }
+
+        // GET: Lue/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Lue/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,TimeToMaster,TimeSpent,Source,StartLearningDate,InProgress,CompletionDate")] Topic topic)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(topic);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(topic);
+        }
+
+        // GET: Lue/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topic.FindAsync(id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+            return View(topic);
+        }
+
+        // POST: Lue/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,TimeToMaster,TimeSpent,Source,StartLearningDate,InProgress,CompletionDate")] Topic topic)
+        {
+            if (id != topic.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(topic);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TopicExists(topic.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(topic);
+        }
+
+        // GET: Lue/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var topic = await _context.Topic
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (topic == null)
+            {
+                return NotFound();
+            }
+
+            return View(topic);
+        }
+
+        // POST: Lue/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var topic = await _context.Topic.FindAsync(id);
+            _context.Topic.Remove(topic);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TopicExists(int id)
+        {
+            return _context.Topic.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Jatkuvan MVC/Models/Topic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Jatkuvan_MVC.Models
+{
+    public partial class Topic
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int? TimeToMaster { get; set; }
+        public int? TimeSpent { get; set; }
+        public string Source { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? StartLearningDate { get; set; }
+        public bool? InProgress { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? CompletionDate { get; set; }
+    }
+}
